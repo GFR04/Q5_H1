4ac9287e637de960ce0e519dfd977e653cf23e44
GFR04 <145131047+GFR04@users.noreply.github.com>
2024-08-14 13:26:43 -0300

    Add files via upload

---
+++ Questão5_H1/Controllers/PessoaController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Questão5_H1.Controllers
+{
+        [ApiController]
+    [Route("Api/pessoa")]
+    public class PessoaController : ControllerBase
+    {
+        [HttpPost]
+        [Route("CalcularIMC")]
+
+        public IActionResult CalcularIMC(Pessoa _pessoa)
+        {
+            if (_pessoa.Altura <= 0 || _pessoa.Peso <= 0)
+            {
+                return BadRequest("Peso e altura devem ser maiores que zero");
+            }
+            return Ok($"O valor do seu IMC é {_pessoa.Nome} é {_pessoa.CalcularIMC().ToString("F2")}");
+        }
+        [HttpPost]
+        [Route("ConsultarTabelaIMC")]
+        public IActionResult ConsultarTabelaIMC (Pessoa _pessoa)
+        {
+            if (_pessoa.Altura <= 0 || _pessoa.Peso <= 0) 
+            {
+                return BadRequest("Peso e altura devem ser maior de zero.");
+            }
+            double IMC = _pessoa.CalcularIMC();
+            if (IMC < 18.5)
+            {
+                return Ok("Muito abaixo do peso.");
+            }
+
+            else if (IMC >= 18.6 && IMC <= 24.9)
+            {
+                return Ok("Abaixo do peso");
+            }
+
+            else if (IMC >= 25 && IMC <= 29.9)
+            {
+                return Ok("Acima do peso");
+            }
+
+            else if (IMC >= 30 && IMC <= 34.9)
+            {
+                return Ok("Obsedidade grau I");
+            }
+
+            else if (IMC >= 35 && IMC <= 39.9)
+            {
+                return Ok("Obsedidade grau II");
+            }
+            return Ok("Obesidade grau III");
+        }
+    }
+}
---
+++ Questão5_H1/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿namespace Questão5_H1.Controllers
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Peso { get; set; }
+        public double Altura { get; set; }
+
+        public double CalcularIMC()
+        {
+            return Peso / (Altura * Altura);
+        }
+    }
+}
